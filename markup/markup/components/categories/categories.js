39ef69040401a661afb44ee62e290f47a80724fe
Zmievskoy Andrew <a.zmievskoy@digitalwand.ru>
2016-06-28 17:20:47 +0300

    верстка страницы подразделений

--- markup/markup/components/categories/categories.js
+++ markup/markup/components/categories/categories.js
@@ -2,5 +2,11 @@ $(document).ready(function () {
 
     $('.js-categories').select2();
 
+    $('.js-categories-structure').select2({
+        // allowClear: true,
+        // tags: true,
+        placeholder: ' Подразделение '
+    });
+
 });
 
