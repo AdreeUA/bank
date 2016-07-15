d2f0222715ef0c8f217bc1c431065aaccc51d562
Zmievskoy Andrew <a.zmievskoy@digitalwand.ru>
2016-07-15 11:04:13 +0300

    изменил иконки, правил осн меню

--- markup/markup/components/main-nav/main-nav.js
+++ markup/markup/components/main-nav/main-nav.js
@@ -1,4 +1,26 @@
-
+//
+//
+// $(document).ready( function () {
+//
+//     const $submenu = $('.main-nav__submenu'),
+//         $menu = $('.main-nav__list'),
+//         $menuWidth = $menu.width(),
+//         $menuLeftPos = $menu.offset().left,
+//         $menuRightPos = $menuLeftPos + $menuWidth;
+//
+//     $(this).find($submenu).each(function () {
+//         const $submenuLeftPos = $(this).offset().left,
+//             $submenuWidth = $(this).width(),
+//             $submenuRightPos = $submenuLeftPos + $submenuWidth,
+//             $value = $submenuRightPos - $menuRightPos;
+//
+//         if ( $value > 0 ) {
+//             $(this).css('left', -$value - 2);
+//         }
+//
+//     });
+//
+// });
 
 $(document).ready( function () {
 
@@ -10,9 +32,11 @@ $(document).ready( function () {
 
     $(this).find($submenu).each(function () {
         const $submenuLeftPos = $(this).offset().left,
-            $submenuWidth = $(this).width(),
+            $submenuWidth = $(this).children().width(),
             $submenuRightPos = $submenuLeftPos + $submenuWidth,
             $value = $submenuRightPos - $menuRightPos;
+
+        console.log($(this).children().width());
 
         if ( $value > 0 ) {
             $(this).css('left', -$value - 2);
