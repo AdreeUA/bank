bf19e1e1533af773741cfcfd92f87e189cc4ad69
Zmievskoy Andrew <a.zmievskoy@digitalwand.ru>
2016-07-19 17:38:10 +0300

    верстка новой страницы подарков

---
+++ markup/markup/components/gift-promo/data/data.js
@@ -0,0 +1,31 @@
+data = { giftPromo: {
+    large: [
+    {
+        image: "bank-gift.png",
+        title: "Самый простой способ исполнения желаний"
+    }],
+    long: [
+        {
+        image: "gift-culture.png",
+        title: "Личный успех – общая гордость"
+    }],
+    small: [
+        {
+        image: "gift.png"
+    }],
+    feedback: [
+        {
+        icon: "icon-phone-large.png",
+        title: "ОБРАТНАЯ СВЯЗЬ",
+        name: "phone"
+    }],
+    personaOffice: [
+        {
+        icon: "icon-arrow-large.png",
+        title: "ПЕРЕЙТИ В ЛИЧНЫЙ КАБИНЕТ",
+        name: "arrow"
+    }]
+
+
+
+}};
